ae3c93550c2d0c446e5528b28adad4d33da5f31a
yuru108 <Changapple0736@gmail.com>
2024-04-02 16:30:54 +0800

    add SELECT

--- connect_db.py
+++ connect_db.py
@@ -8,7 +8,20 @@ connection = mysql.connector.connect(host='127.0.0.1',
 
 cursor = connection.cursor()
 
-cursor.execute('SELECT * FROM `course`;')
+# cursor.execute('')
+
+# for i in range(1, 71):
+#     cursor.execute("INSERT INTO `schedule_D1150459` (`time_id`) VALUES (%s)", (i,))
+
+# connection.commit()
+
+input_SID = 'D1150080'
+input_CID = '2'
+
+table = 'course'
+
+cursor.execute(f'SELECT * FROM `{table}` 
+               WHERE `ID` = {input_CID};')
 
 records = cursor.fetchall()
 for r in records:
