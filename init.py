d236bee5e8f7d2e48435a1149df0b6041019e5d8
yuru108 <Changapple0736@gmail.com>
2024-04-03 15:54:41 +0800

    add comment

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 class Student:
     def __init__(self, SID, name, major):
-        self.SID = SID
+        self.SID = SID                  # 學號
         self.name = name
         self.major = major
 
@@ -12,17 +12,17 @@ class Time:
 
 class Course:
     def __init__(self, ID, course_id, cname, professor, type, major, credit, max_member, current_member, start, end):
-        self.ID = ID
-        self.course_id = course_id
-        self.cname = cname
-        self.professor = professor
-        self.type = type
+        self.ID = ID                    # 選課代碼
+        self.course_id = course_id      # 課程代碼
+        self.cname = cname              # 課堂名稱
+        self.professor = professor      # 教授(id)
+        self.type = type                # 必修/選修/通識
         self.major = major
-        self.credit = credit
+        self.credit = credit            # 學分
         self.max_member = max_member
         self.current_member = current_member
-        self.start = start
-        self.end = end
+        self.start = start              # 開始時間(id)
+        self.end = end                  # 結束時間(id)
 
 class Professor:
     def __init__(self, PID, name, major):
@@ -33,4 +33,4 @@ class Professor:
 class Schedule:
     def __init__(self, time_id, course_id):
         self.time_id = time_id
-        self.course_id = course_id
+        self.course_id = course_id      # 選課代碼
