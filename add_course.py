1d96a0ce89588f9da49a05356350c0d4dcbe91a2
Styliz77 <126126781+Styliz77@users.noreply.github.com>
2024-04-03 13:08:48 +0800

    Update add_course.py

--- add_course.py
+++ add_course.py
@@ -1,7 +1,12 @@
-from connect_db import student_data
+from connect_db import student_data,course_data,select_table
 
 SID = 'D1150080'
 
 print(student_data(SID))
 print(student_data(SID)[0])
 print(student_data(SID)[0][1])
+flag = 1
+CID = '0002'
+if student_data.major != course_data.major and course_data.major != none
+    flag = 0
+    
